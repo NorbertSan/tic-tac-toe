10847b731136eeef3fcf78788ac3280a3f5f57a2
Norbert Sanpruch <norbert.sanpruch@miquido.com>
2021-05-18 18:38:21 +0200

    feat(game): create simple Game,Player,GameStatus classes

---
+++ src/main/java/com/example/tictactoe/model/Game.java
@@ -0,0 +1,10 @@
+package com.example.tictactoe.model;
+
+public class Game {
+
+    private String gameId ;
+    private Player player1;
+    private Player player2;
+    private GameStatus status;
+    private int [][] board;
+}
---
+++ src/main/java/com/example/tictactoe/model/Player.java
@@ -0,0 +1,9 @@
+package com.example.tictactoe.model;
+
+import lombok.Data;
+
+@Data
+public class Player {
+
+    private String login;
+}

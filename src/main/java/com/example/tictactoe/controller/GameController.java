94d4c0eef426c92a21f74241f3cf393ec7be411e
Norbert Sanpruch <norbert.sanpruch@miquido.com>
2021-05-25 19:11:52 +0200

    feat(gamecontroller): add connect to game endpoint: POST /game/id/connect

--- src/main/java/com/example/tictactoe/controller/GameController.java
+++ src/main/java/com/example/tictactoe/controller/GameController.java
@@ -1,15 +1,16 @@
 package com.example.tictactoe.controller;
 
+import com.example.tictactoe.exception.GameException;
 import com.example.tictactoe.model.Game;
 import com.example.tictactoe.model.Player;
 import com.example.tictactoe.service.GameService;
+import com.example.tictactoe.storage.GameStorage;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -22,6 +23,12 @@ public class GameController {
     @PostMapping("")
     public ResponseEntity<Game> create(@RequestBody Player player){
         Game game = gameService.createGame(player);
+        return ResponseEntity.ok(game);
+    }
+
+    @PostMapping("/:id/connect")
+    public ResponseEntity<Game> connectToGame(@RequestBody Player player2, @PathVariable String id) throws GameException {
+        Game game = gameService.connectToGame(player2,id);
         return ResponseEntity.ok(game);
     }
 }

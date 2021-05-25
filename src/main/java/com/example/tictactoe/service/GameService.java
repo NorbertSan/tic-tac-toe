61fb107eb6e2163b745504a8c9e4d1a08e9c42de
Norbert Sanpruch <norbert.sanpruch@miquido.com>
2021-05-25 18:47:45 +0200

    feat(gameservice): add checkWinner method

--- src/main/java/com/example/tictactoe/service/GameService.java
+++ src/main/java/com/example/tictactoe/service/GameService.java
@@ -58,8 +58,43 @@ public class GameService {
 
         int[][] board = game.getBoard();
         board[move.getPositionX()][move.getPositionY()] = move.getSign().getValue();
+        game.setBoard(board);
 
+        checkWinner(game.getBoard(),move.getSign());
         return game;
+    }
+
+    private Boolean checkWinner(int[][] board, Sign playerSign){
+        int [] oneDimensionArr = new int[9];
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[i].length;j++){
+                oneDimensionArr[i*j] = board[i][j];
+            }
+        }
+
+        int [][] winCombitations = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{1,5,8},{2,5,8}};
+
+        boolean hasPlayerWon = false;
+        for(int k=0; k< winCombitations.length; k++){
+            int [] singleWinCombination = winCombitations[k];
+            int a = singleWinCombination[0];
+            int b = singleWinCombination[1];
+            int c = singleWinCombination[2];
+
+            if(hasPlayerWon){
+                break;
+            }
+
+            if(
+                    oneDimensionArr[a] == playerSign.getValue() &&
+                            oneDimensionArr[b] == playerSign.getValue() &&
+                                oneDimensionArr[c] == playerSign.getValue()
+            ){
+                hasPlayerWon=true;
+            }
+        }
+
+        return hasPlayerWon;
     }
 
     private Game getGame(String gameId) throws GameException{
